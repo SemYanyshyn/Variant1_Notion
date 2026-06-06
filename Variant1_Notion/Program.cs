78d75516ca93eb682409731fb41ca8017586fbb2
Semen <a860@MacBook-Pro-2.local>
2026-06-06 11:59:34 +0300

    adding comments to class Program

--- Variant1_Notion/Program.cs
+++ Variant1_Notion/Program.cs
@@ -97,7 +97,6 @@ public static class ExamProcessor
         return new XElement ("TaskA", query);
 }}
 
-
 public static class DataLoader
 {
     public static IEnumerable<Student> StudentLoader(string path)
@@ -105,9 +104,105 @@ public static class DataLoader
         XDocument document = XDocument.Load(path);
 
         var students =
-            
+            from student in document.Root.Elements("Student")
+            select new Student(
+                (int)student.Element("Id"),
+                (int)student.Element("GroupName"),
+                (string)student.Element("Surname")
+            );
+
+        return students;
+
     }
 
+    public static IEnumerable<Group> GroupLoader(string path)
+    {
+        XDocument document = XDocument.Load(path);
+
+        var groups = 
+        from groupItem in document.Root.Elements("Group")
+        select new Group(
+            (int)groupItem.Element("Id"),
+            (string)groupItem.Element("GroupName")
+        );
+        return groups;
+    }    
+
+
+    public static IEnumerable<Subject> SubjectLoader(string path)
+    {
+        XDocument document = XDocument.Load(path);
+        
+        var subjects = 
+        from subject in document.Root.Elements("Subject")
+        select new Subject(
+            (int)subject.Element("Id"),
+            (string)subject.Element("Name"),
+            (int)subject.Element("Credits")
+        );
+        return subjects;
+    }
+
+    public static IEnumerable<Exam> ExamLoader(string path)
+    {
+        XDocument document = XDocument.Load(path);
+
+        var exams = 
+        from exam in document.Root.Elements("Exam")
+        select new Exam(
+            (int)exam.Element("StudentId"),
+            (int)exam.Elements("SubjectId"),
+            (int)exam.Elements("Score")
+        );
+        return exams;
+    }
+}
+
+
+
+public class Program
+{
+    public static void Main()
+    {
+        // AppContext.BaseDirectory — це папка, звідки запускається програма [bin/Debug/net10.0/]
+        string dataDirectory = AppContext.BaseDirectory;
+
+        string studentsPath = Path.Combine(dataDirectory, "students.xml"); // Це створює повний шлях до файлу students.xml.
+        string groupsPath = Path.Combine(dataDirectory, "groups.xml");
+        string subjectsPath = Path.Combine(dataDirectory, "subjects.xml");
+
+// Програма запускається.
+// AppContext.BaseDirectory визначає папку запуску.
+// Path.Combine створює повні шляхи до XML-файлів.
+// Ці шляхи передаються в DataLoader.
+// DataLoader відкриває XML-файли.
+// XML-дані перетворюються в об’єкти Student, Group, Subject.
+
+        IEnumerable<string> examPaths = new string[] // examPaths - Це колекція рядків. new string[] - створює список  "шлях до exam1.xml","шлях до exam2.xml".
+        {
+            Path.Combine(dataDirectory, "exam1.xml"), // Склеює папку запуску програми з назвою файлу
+            Path.Combine(dataDirectory, "exam2.xml")
+        };
+
+        // Ці рядки викликають методи з DataLoader і зчитують дані з XML-файлів і записують значення у C# об’єкти.
+        IEnumerable<Student> students = DataLoader.StudentLoader(studentsPath);
+        IEnumerable<Group> groups = DataLoader.GroupLoader(groupsPath);
+        IEnumerable<Subject> subjects = DataLoader.SubjectLoader(subjectsPath);
+        IEnumerable<Exam> exams = DataLoader.ExamLoader(examPaths);
+
+        XElement result = ExamProcessor.TaskA( // передаю дані методу TaskA і він викликається 
+            students,
+            subjects,
+            groups,
+            exams
+        );
+
+        string resultPath = Path.Combine(dataDirectory, "taskAResult.xml"); // зберігає результат у XML-файл і показує його в консолі
+
+        new XDocument(result).Save(resultPath);
+
+        Console.WriteLine(result);
+    }
 }
 
 
@@ -120,10 +215,7 @@ public static class DataLoader
 
 
 
-
-
-
-
+ 
 
 
 
